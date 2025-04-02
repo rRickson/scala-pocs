57ec39d76bd939dc732b84479b8b11eef2bef07c
Rickson Vasconcelos <rickson.vasc@gmail.com>
2025-04-02 15:20:25 -0300

    Ref

--- const-killer/src/main/scala/approaches/elegantObject/raw/person/main.scala
+++ const-killer/src/main/scala/approaches/elegantObject/raw/person/main.scala
@@ -6,10 +6,10 @@ import com.rRickson.approaches.elegantObject.traits.{Active, Blocked}
 class main {
 
   def main(): Unit = {
-    val card1 = new User("Alice", new Active)
-    val card2 = new User("Bob", new Blocked)
+    val person1 = new User("Alice", new Active)
+    val person2 = new User("Bob", new Blocked)
 
-    println(card1.canLogin)
-    println(card2.canLogin)
+    println(person1.canLogin)
+    println(person2.canLogin)
   }
 }
